032e0b89d2a7d8025c3136de11ace59cd05f460c
Siddharth Srivastava <siddharthsrivastava76@gmail.com>
2022-04-02 13:51:42 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from PIL import Image
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+    
+@app.route('/classify/', methods=['POST'])
+def classify():
+    f = request.files['img']
+    filename = secure_filename(f.filename) 
+    f.save(filename)
+    im = Image.open(filename).convert("RGB")
+
+    pixels = [im.getpixel((i,j)) for j in range(im.height) for i in range(im.width)]
+
+    cond = (255, 194, 231) in pixels
+    os.remove(filename)
+    if cond:
+        return "Peppa Pig"
+    else:
+        return "Assassin's Creed"
+
+if __name__ == '__main__':
+    app.run()
